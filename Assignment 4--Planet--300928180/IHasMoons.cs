b3db855d3c991cece5718ce2ffa443a026954a51
Vishw Patel <vishwpatel511@gmail.com>
2017-07-22 19:51:48 -0400

    Public interface IHasMoons has been created

---
+++ Assignment 4--Planet--300928180/IHasMoons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4__Planet__300928180
+{
+    /*
+   Name: Vishw Patel
+   Student ID: 300928180
+   Date Modified: July 22, 2017
+   Description: Assignment 4
+   version: 0.2 - Created Public Interface IHasMoons */
+
+
+    public interface IHasMoons
+    {
+        /// <summary>
+        ///  Calling a HasMoon() method from GiantPlanet class
+        /// </summary>
+
+
+        bool HasMoons();
+    }
+
+}
