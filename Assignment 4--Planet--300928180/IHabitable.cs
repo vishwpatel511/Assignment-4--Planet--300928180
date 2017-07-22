556bc20a1d9ad9ea5f06444d205bda86d9b36113
Vishw Patel <vishwpatel511@gmail.com>
2017-07-22 19:58:37 -0400

    Public interface IHabitable has been created..

---
+++ Assignment 4--Planet--300928180/IHabitable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4__Planet__300928180
+{
+    /*
+    Name: Vishw Patel
+    Student ID: 300928180
+    Date Modified: July 22, 2017
+    Description: Assignment 4 
+    version: 0.2 Final - Created public interface IHabitable
+
+*/
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
