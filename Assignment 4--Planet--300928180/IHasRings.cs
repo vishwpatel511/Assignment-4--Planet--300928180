d56024f7496a02bfef3a99df9fe240b233dae004
Vishw Patel <vishwpatel511@gmail.com>
2017-07-22 19:55:00 -0400

    Public interface IHasRings has been created...

---
+++ Assignment 4--Planet--300928180/IHasRings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4__Planet__300928180
+{
+    /*
+  Name: Vishw Patel
+  Student ID: 300928180
+  Date Modified: July 22, 2017
+  Description: Assignment 4
+  version: 0.2 - Created Public Interface IHasRings */
+
+    public interface IHasRings
+    {
+        /// <summary>
+        /// Calling HasRings() method
+        /// </summary>
+        /// <returns></returns>
+        bool HasRings();
+
+    }
+
+}
