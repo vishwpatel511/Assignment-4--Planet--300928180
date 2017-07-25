f2e59a12acd5faf3109a072102e5218e2d54ed9c
Vishw Patel <vishwpatel511@gmail.com>
2017-07-25 18:21:59 -0400

    Display additional classes for accurate output..
    Display HasMoons(), HasRings() and IHabitable() class

--- Assignment 4--Planet--300928180/Program.cs
+++ Assignment 4--Planet--300928180/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +9,9 @@ namespace Assignment_4__Planet__300928180
     /*
  Name: Vishw Patel
  Student ID: 300928180
- Date Modified: July 22, 2017
+ Date Modified: July 25, 2017
  Description: Assignment 4 
- version: 0.2 Final - Created Program class to display the result..
-
+ version: 0.2 Final - Created and edited Program class to display the more accurate result..
  */
     /// <summary>
     /// That's the driver class
@@ -24,18 +23,26 @@ namespace Assignment_4__Planet__300928180
             // creating giantPlanet object
             GiantPlanet giantPlanet = new GiantPlanet("Saturn", 116464, 5.683E16, "Gaseous");
             //67, 11.86, 4, 0.41,
-            Console.WriteLine(giantPlanet.ToString()); 
+            Console.WriteLine(giantPlanet.ToString());
+            // Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Does the Planet has moons? " + giantPlanet.HasMoons());
+            Console.WriteLine("Does the Planet has rings? " + giantPlanet.HasRings());
             Console.WriteLine("--------------------------------------------------------");
 
-             // creating terrestrialPlanet object
+
+            // creating terrestrialPlanet object
 
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Venus", 12104, 4.867E24, true);
             Console.WriteLine(terrestrialPlanet.ToString());
-            
+           // Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Does the Planet has moons? " + terrestrialPlanet.HasMoons());
+            Console.WriteLine("Does the Planet has oxygen and is Habitable? " + terrestrialPlanet.Habitable());
+
+
             // Method calling
             WaitForAnyKey();
         }
-         
+
         /// <summary>
         /// WaitForAnyKey() method to hold the console screen 
         /// </summary>
